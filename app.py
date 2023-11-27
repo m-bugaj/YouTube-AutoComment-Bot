d17b69ae69dab1e106dad50a21f6c4f06f7b652a
bugaj55113 <bbugi2016@gmail.com>
2023-11-27 21:31:28 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pickle
+import os
+import random
