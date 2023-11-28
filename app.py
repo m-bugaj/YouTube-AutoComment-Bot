ba922af9af6815b7bd0a2cf4c74f6b4982ba6754
bugaj55113 <bbugi2016@gmail.com>
2023-11-28 12:23:06 +0100

    Added cookies handling and closed browser window

--- app.py
+++ app.py
@@ -5,3 +5,29 @@ import time
 import pickle
 import os
 import random
+
+def is_cookies_empty(filename="cookies.pkl"):
+    # Check if the cookies file is empty
+    return not os.path.exists(filename) or os.path.getsize(filename) == 0
+
+# Initialize the browser with undetected_chromedriver
+driver = uc.Chrome()
+
+if is_cookies_empty():
+    # Open the login page, user logs in manually
+    driver.get("https://www.youtube.com/")
+    input('Log in to your Google account and press Enter to continue...')
+    
+    # Save cookies to the file
+    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+else:
+    # Load cookies if available
+    driver.get("https://www.youtube.com/")
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+# Wait a moment for the window to close
+time.sleep(5)
+
+driver.close()
