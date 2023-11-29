204386ec2a4c04205b2294e29ecca2d730e5d155
bugaj55113 <bbugi2016@gmail.com>
2023-11-29 22:04:22 +0100

    Added automation to visit each channel from channel_urls.txt and navigate to comments section

--- app.py
+++ app.py
@@ -10,6 +10,43 @@ def is_cookies_empty(filename="cookies.pkl"):
     # Check if the cookies file is empty
     return not os.path.exists(filename) or os.path.getsize(filename) == 0
 
+def leave_a_comment(channel_url):
+    # Wait a moment to allow the page to load
+    time.sleep(2)
+
+    # Go to the channel
+    driver.get(channel_url)
+
+    time.sleep(2)
+
+    # Go to the videos section
+    try:
+        videos_tab = driver.find_elements(By.XPATH, '//*[@id="tabsContent"]/yt-tab-group-shape/div[1]/yt-tab-shape[2]')
+        if videos_tab:
+            videos_tab[-1].click()
+    except Exception as e:
+        print(f"An error occurred while switching to videos tab: {e}")
+
+    time.sleep(2)
+
+    # Go to the first video
+    element = driver.find_element(By.CSS_SELECTOR, '#contents > ytd-rich-item-renderer:nth-child(1)')
+    element.click()
+
+    time.sleep(3)
+
+    # Scroll slightly to load the commenting section
+    driver.execute_script("window.scrollBy(0, 500);")
+
+    # Delay to load the comments section
+    time.sleep(4)
+
+    # Click on the comment input box
+    comment_input_box = driver.find_elements(By.XPATH, '//*[@id="placeholder-area"]')
+    comment_input_box[-1].click()
+
+    time.sleep(1)
+
 # Initialize the browser with undetected_chromedriver
 driver = uc.Chrome()
 
@@ -26,6 +63,15 @@ else:
     cookies = pickle.load(open("cookies.pkl", "rb"))
     for cookie in cookies:
         driver.add_cookie(cookie)
+
+yt_channels = []
+with open("channel_urls.txt", 'r') as file:
+    for line in file:
+        cleaned_line = line.strip()
+        yt_channels.append(cleaned_line)
+
+for channel_url in yt_channels:
+    leave_a_comment(channel_url)
 
 # Wait a moment for the window to close
 time.sleep(5)
