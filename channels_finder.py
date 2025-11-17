2ea5a40bec30d8e1b9ea838bbda4af8418a411f2
m-bugaj <bbugi2016@gmail.com>
2025-11-17 22:03:07 +0100

    Implement part of the functionality for finding potential commentator channels

---
+++ channels_finder.py
@@ -0,0 +1,46 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pickle
+import os
+import random
+import sys
+from utils.youtube_navigation import go_to_videos_tab, get_latest_videos, reject_cookies
+
+class ChannelsFinder:
+    def __init__(self):
+        self.driver = uc.Chrome()
+
+    def save_new_channel_urls(new_channel_urls):
+        return
+
+    def find_comentators_for_channel(self, channel_url, videos_count_to_handle=1):
+        time.sleep(2)
+
+        # Go to the channel
+        self.driver.get(channel_url)
+
+        go_to_videos_tab(self.driver)
+        videos = get_latest_videos(self.driver, videos_count_to_handle)
+
+
+
+
+        return
+
+    def run(self):
+        videos_count_to_handle = 2
+        yt_channels = []
+        with open("channel_urls.txt", 'r') as file:
+            for line in file:
+                cleaned_line = line.strip()
+                yt_channels.append(cleaned_line)
+
+        self.driver.get('https://www.youtube.com/')
+        reject_cookies(self.driver)
+
+        for channel_url in yt_channels:
+            self.find_comentators_for_channel(channel_url, videos_count_to_handle)
