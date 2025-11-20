c72d5a5c3e080b0f068724387e231ed09599c9fe
m-bugaj <bbugi2016@gmail.com>
2025-11-20 23:34:52 +0100

    Fetch comentators from the video

--- channels_finder.py
+++ channels_finder.py
@@ -9,13 +9,29 @@ import os
 import random
 import sys
 from utils.youtube_navigation import go_to_videos_tab, get_latest_videos, reject_cookies
-
+from utils.common_navigation import scroll_to_bottom
 class ChannelsFinder:
     def __init__(self):
         self.driver = uc.Chrome()
 
     def save_new_channel_urls(new_channel_urls):
         return
+    
+    def get_commentators_channels(self):
+        authors = self.driver.find_elements(By.CSS_SELECTOR, "a#author-text")
+        urls = []
+
+        for a in authors:
+            try:
+                href = a.get_attribute("href")
+                if href and "/@" in href or "/channel/" in href:
+                    urls.append(href)
+            except:
+                continue
+
+        urls = list(set(urls))
+        return urls
+
 
     def find_comentators_for_channel(self, channel_url, videos_count_to_handle=1):
         time.sleep(2)
@@ -25,7 +41,15 @@ class ChannelsFinder:
 
         go_to_videos_tab(self.driver)
         videos = get_latest_videos(self.driver, videos_count_to_handle)
+        for video in videos:
+            video.click()
+            time.sleep(5)
+            scroll_to_bottom(self.driver)
+            urls = self.get_commentators_channels()
+            print(urls)
+            self.driver.back()
 
+        time.sleep(5)
 
 
 
